a878a4a1f0b9d4d204898bff707f49d5d1ee65ea
alumno <alumno@Instituto.local>
2023-09-15 09:01:21 -0300

    ex

---
+++ clase.js
@@ -0,0 +1,33 @@
+function salariodeJuan ()
+{
+    let salarioanual = 21600
+    return 'salario de juan'+ salario / 12
+}
+
+console.log(salariodeJuan () )
+
+
+function notas( a, b, c)
+{
+    not1 =a
+    not2 =b
+    not3 =c
+    resultado = not1 + not2 + not3 
+return 'promedio'+ resultado / 3
+
+let notas = prompt ('resultado de num1')
+let notas = prompt ('resultado de num2')
+let notas = prompt ('resultado de num3')
+
+}
+
+console.log(promedio ('promedio de las notas'+ not1 + not2 + not3 ))
+alert(promedio(not1, not2, not3))
+
+function marcos()
+{
+    let salariomensual = 1500
+    return 'salario de marcos es de'+ salariomensual * 12
+}
+alert(salariomarcos())
+console.log (salariomarcos())
